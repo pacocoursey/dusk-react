55fc12f64771a535f68ac5aab44000a078e5d7a3
pacocoursey <pvco.coursey@gmail.com>
2019-01-15 22:47:54 -0700

    fix sketch icon

--- src/icons/sketch.js
+++ src/icons/sketch.js
@@ -10,9 +10,9 @@ const Sketch = (props) => {
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={size} height={size}>
       <circle fill={bg} cx="256" cy="256" r="256" />
       <path fill="none" stroke={fg} strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M256 371.369L128.5 223.222l55.233-74.848L256 140.631l72.267 7.743 55.233 74.848z" />
-      <path fill="none" stroke="#FFF" strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M256 371.369L128.5 223.222h255z" />
-      <path fill="none" stroke="#FFF" strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M256 371.369l-75.88-148.147h151.76zM256 140.631l-75.88 82.591h151.76z" />
-      <path fill="none" stroke="#FFF" strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M183.733 148.374l-3.613 74.848L256 140.631zM328.267 148.374l3.613 74.848L256 140.631z" />
+      <path fill="none" stroke={fg} strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M256 371.369L128.5 223.222h255z" />
+      <path fill="none" stroke={fg} strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M256 371.369l-75.88-148.147h151.76zM256 140.631l-75.88 82.591h151.76z" />
+      <path fill="none" stroke={fg} strokeWidth="10" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="M183.733 148.374l-3.613 74.848L256 140.631zM328.267 148.374l3.613 74.848L256 140.631z" />
     </svg>
   );
 };
